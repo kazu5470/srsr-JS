d0341b55fce2e4a47ddd8184713ef0954418cc9a
kazu5470 <105265063+kazu5470@users.noreply.github.com>
2022-11-15 10:26:55 +0000

    for_of

---
+++ for_of.js
@@ -0,0 +1,9 @@
+const fruits = ["りんご","みかん","バナナ"];
+
+// for...ofを使うと自動的に配列の要素数回だけ繰り返し処理をする
+// for (const 変数 of 配列){
+//     console.log(変数)    
+// }
+for (const fruit of fruits) {
+    console.log(fruit);
+}    
