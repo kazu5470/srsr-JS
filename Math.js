ab1fbf009d53a2964f50e48a3cf7da93d362dd35
kazu5470 <105265063+kazu5470@users.noreply.github.com>
2022-11-19 14:35:52 +0000

    Math

--- Math.js
+++ Math.js
@@ -5,3 +5,8 @@ console.log(Math.PI); //3.141592653589793
 console.log(Math.abs(-10));  // 10
 console.log(Math.abs(10));  // 10
 
+// 小数点の処理
+console.log(Math.round(1.4)); // 四捨五入
+console.log(Math.round(1.5)); // 四捨五入
+console.log(Math.floor(10.3));  // 切り下げ
+console.log(Math.ceil(10.3));  //切り上げ
