614a05182c4672e116ec00926a1c9c1e26793dbf
kazu5470 <105265063+kazu5470@users.noreply.github.com>
2022-11-22 13:26:45 +0000

    Math

--- Math.js
+++ Math.js
@@ -10,3 +10,11 @@ console.log(Math.round(1.4)); // 四捨五入
 console.log(Math.round(1.5)); // 四捨五入
 console.log(Math.floor(10.3));  // 切り下げ
 console.log(Math.ceil(10.3));  //切り上げ
+
+// ランダムの数を生成
+console.log(Math.random()); // randomメソッドは０以上１未満の数字をランダムに生成する
+
+// サイコロを作成
+console.log(Math.floor(Math.random() * 6) + 1);
+// Math.floorで小数点以下を切り捨て、Math.randomでランダムの数を生成し*6とすることで０以上６未満の数を出す
+// 最後に+1することで０以上６以下の数字を出す
