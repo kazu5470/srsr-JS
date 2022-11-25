cd2888efc82ce9137c2020c929e4be8a83dd1190
kazu5470 <105265063+kazu5470@users.noreply.github.com>
2022-11-25 14:39:18 +0000

    newdate

--- newdate.js
+++ newdate.js
@@ -8,11 +8,12 @@ console.log(now); // 現在の日付、時刻が表示される
 const theDay = new Date(2030, 3, 1, 5, 20, 0);
 console.log(theDay); // Mon Apr 01 2030 05:20:00 GMT+0900(日本標準時)
                      // 引数を３にしたのにApr(4月)となっている点については言語仕様上のルールのため、注意する
+                     // 月が0〜１１になるため
 
 // Dateオブジェクトのメソッド
 const theDay2 = new Date(2030, 3, 9, 5, 20, 0);
-console.log(theDay2.getFullYear());
-console.log(theDay2.getMonth());
-console.log(theDay2.getDate());
+console.log(theDay2.getFullYear()); // 2030
+console.log(theDay2.getMonth()); // 3
+console.log(theDay2.getDate()); // 9
 
 
