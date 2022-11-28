fd04bbe1266a5d91b97cd5977260311905bd7f75
kazu5470 <105265063+kazu5470@users.noreply.github.com>
2022-11-28 11:53:27 +0000

    newdate

--- newdate.js
+++ newdate.js
@@ -17,3 +17,17 @@ console.log(theDay2.getMonth()); // 3
 console.log(theDay2.getDate()); // 9
 
 
+// 今日の日付をわかりやすく表示する
+const today = new Date().toLocaleString('ja-JP', {
+    dateStyle: 'long'
+});
+console.log(today); // 2022年11月28日
+
+const formatter = Intl.DateTimeFormat('ja-JP', {
+    dateStyle: 'long'
+});
+
+const today2 = formatter.format(new Date());
+console.log(today2); // 2022年11月28日
+
+
